dc03b600239f6fb31c67005ae29f69a200785b72
thefigtree <ldj8431@naver.com>
2024-11-23 15:33:32 +0900

    Feat: 추가 Shadcn LocationDialog

--- src/app/dashboard/locations/_components/location-button.tsx
+++ src/app/dashboard/locations/_components/location-button.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { PlusIcon } from "lucide-react";
 import { useState } from "react";
+import LocationDialog from "./location.dialog";
 
 export default function LocationButton() {
   const [open, setOpen] = useState(false);
@@ -14,6 +15,8 @@ export default function LocationButton() {
           <PlusIcon className="mr-2"></PlusIcon>
           위치를 추가해야 합니다.
         </Button>
+
+        <LocationDialog open={open} setOpen={setOpen}></LocationDialog>
       </div>
     </div>
   );
---
+++ src/app/dashboard/locations/_components/location.dialog.tsx
@@ -0,0 +1,30 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+type Props = {
+  id?: string | null;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function LocationDialog({ id = null, open, setOpen }: Props) {
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
