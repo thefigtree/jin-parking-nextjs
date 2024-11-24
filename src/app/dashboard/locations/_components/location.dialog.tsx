894a5c6ffac1b44b34072f6093c54ed94b394789
thefigtree <ldj8431@naver.com>
2024-11-24 23:59:31 +0900

    Feat: 추가 Location Dialog Submit Button UI, 추가 HandleSubmit 함수

--- src/app/dashboard/locations/_components/location.dialog.tsx
+++ src/app/dashboard/locations/_components/location.dialog.tsx
@@ -1,11 +1,19 @@
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
+import { useSpotStore } from "@/store";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const totalSteps = 4;
+
+const stepIncrement = 100 / totalSteps;
 
 type Props = {
   id?: string | null;
@@ -14,16 +22,56 @@ type Props = {
 };
 
 export default function LocationDialog({ id = null, open, setOpen }: Props) {
+  const [step, setStep] = useState(1);
+  const [submit, setSubmit] = useState(false);
+
+  const SpotStore = useSpotStore();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setStep(1);
+
+    // fetch data
+    const fetchData = () => {
+      console.log("fetch data");
+    };
+
+    if (id && open) {
+      fetchData();
+    } else {
+      SpotStore.restart();
+    }
+  }, [id, open]);
+
+  const handleSubmit = () => {
+    // db에 데이터 저장 함수
+    console.log(SpotStore.data);
+  };
+
+  const handleAnother = () => {};
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogTitle>내 장소 보기</DialogTitle>
           <DialogDescription>
             This action cannot be undone. This will permanently delete your
             account and remove your data from our servers.
           </DialogDescription>
         </DialogHeader>
+
+        <DialogFooter>
+          <div className="flex flex-col mt-4 w-full space-y-2">
+            <Button type="button" onClick={handleSubmit}>
+              등록하기
+            </Button>
+            <Button type="button" variant="outline" onClick={handleAnother}>
+              다른 장소 보기
+            </Button>
+          </div>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
