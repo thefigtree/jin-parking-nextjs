bb892b419dc6a87445efba16176f50e47bfcc9b8
thefigtree <ldj8431@naver.com>
2024-11-27 22:28:09 +0900

    Feat: 추가 Feat: 추가 Switch Button (In Templete) UI

--- src/app/dashboard/locations/template.tsx
+++ src/app/dashboard/locations/template.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import LocationButton from "./_components/location-button";
+import LayoutSwitch from "@/components/layout/layout-switch";
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <LocationButton></LocationButton>
+      <LayoutSwitch></LayoutSwitch>
       {children}
     </div>
   );
--- src/components/layout/layout-switch.tsx
+++ src/components/layout/layout-switch.tsx
@@ -1,3 +1,18 @@
+import { ListIcon, MapIcon } from "lucide-react";
+import Link from "next/link";
+
 export default function LayoutSwitch() {
-  return <div>LayoutSwitch</div>;
+  return (
+    <>
+      <div className="flex justify-end space-x-2 text-yellow-600 p-4">
+        <Link href="/dashboard/locations/tile" className="bg-white p-1 rounded">
+          <ListIcon></ListIcon>
+        </Link>
+
+        <Link href="/dashboard/locations/map" className="bg-white p-1 rounded">
+          <MapIcon></MapIcon>
+        </Link>
+      </div>
+    </>
+  );
 }
