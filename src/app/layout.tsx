7205baa8537183aa8f5d101b33db4a37c5ba799c
thefigtree <ldj8431@naver.com>
2024-12-22 23:33:53 +0900

    Feat: 추가 clerkMiddleWare 함수

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Toaster } from "sonner";
+import { ClerkLoaded, ClerkProvider } from "@clerk/nextjs";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -23,18 +24,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={cn(
-          inter.className,
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={cn(
+            inter.className,
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <ClerkLoaded>{children}</ClerkLoaded>
 
-        <Toaster></Toaster>
-      </body>
-    </html>
+          <Toaster></Toaster>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex flex-col items-center pt-12">
+      <SignUp />
+    </div>
+  );
+}
