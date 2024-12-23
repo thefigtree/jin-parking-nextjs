ae32a80f31c2af93eaad124ea98c60e5d9ae809f
thefigtree <ldj8431@naver.com>
2024-12-24 00:00:09 +0900

    Fix: 고침 Clerk Auth 함수

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,12 @@
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-// import { NextRequest, NextResponse } from "next/server";
-
-// const isProtectedRoute = createRouteMatcher(["mybookings(.*)", "/book/(.*)"]);
-
-// const isAdminRoute = createRouteMatcher(["/dashboard(.*)"]);
-
-// export default clerkMiddleware(async (auth, req: NextRequest) => {
-//   const { userId, redirectToSignIn, sessionClaims } = await auth();
-
-//   if (!userId && (isProtectedRoute(req) || isAdminRoute(req))) {
-//     return redirectToSignIn({ returnBackUrl: req.url });
-//   }
-
-//   if (userId && isAdminRoute(req)) {
-//     if (sessionClaims?.metadata.role === "admin") {
-//       return NextResponse.next();
-//     } else {
-//       return new Response("가입절차가 필요합니다.", { status: 401 });
-//     }
-//   }
-
-//   return NextResponse.next();
-// });
-
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/mybookings(.*)", "/book/(.*)"]);
+const isProtectedRoute = createRouteMatcher(["mybookings(.*)", "/book/(.*)"]);
 
 const isAdminRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export default clerkMiddleware((auth, req: NextRequest) => {
-  const { userId, redirectToSignIn, sessionClaims } = auth();
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+  const { userId, redirectToSignIn, sessionClaims } = await auth();
 
   if (!userId && (isProtectedRoute(req) || isAdminRoute(req))) {
     return redirectToSignIn({ returnBackUrl: req.url });
@@ -41,9 +16,34 @@ export default clerkMiddleware((auth, req: NextRequest) => {
     if (sessionClaims?.metadata.role === "admin") {
       return NextResponse.next();
     } else {
-      return new Response(`You are not authorized`, { status: 401 });
+      return new Response("가입절차가 필요합니다.", { status: 401 });
     }
   }
 
   return NextResponse.next();
 });
+
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { NextRequest, NextResponse } from "next/server";
+
+// const isProtectedRoute = createRouteMatcher(["/mybookings(.*)", "/book/(.*)"]);
+
+// const isAdminRoute = createRouteMatcher(["/dashboard(.*)"]);
+
+// export default clerkMiddleware((auth, req: NextRequest) => {
+//   const { userId, redirectToSignIn, sessionClaims } = auth();
+
+//   if (!userId && (isProtectedRoute(req) || isAdminRoute(req))) {
+//     return redirectToSignIn({ returnBackUrl: req.url });
+//   }
+
+//   if (userId && isAdminRoute(req)) {
+//     if (sessionClaims?.metadata.role === "admin") {
+//       return NextResponse.next();
+//     } else {
+//       return new Response(`You are not authorized`, { status: 401 });
+//     }
+//   }
+
+//   return NextResponse.next();
+// });
